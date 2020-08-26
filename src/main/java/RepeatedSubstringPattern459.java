963fa8b83ade2b358534da426efc64b1bb513f1a
nanphonfy <nanphonfy@gmail.com>
2020-08-26 23:18:40 +0800

    459. 重复的子字符串
    正确解法、KMP待学

--- src/main/java/RepeatedSubstringPattern459.java
+++ src/main/java/RepeatedSubstringPattern459.java
@@ -1,4 +1,5 @@
 /**
+ * https://leetcode-cn.com/problems/repeated-substring-pattern/solution/zhong-fu-de-zi-zi-fu-chuan-by-leetcode-solution/
  * @author nanphonfy(南风zsr)
  * @date 2020/8/24
  */
@@ -42,32 +43,47 @@ public class RepeatedSubstringPattern459 {
         return false;
     }
 
-    public boolean repeatedSubstringPattern2(String s) {
-        for (int i = 1; i <= s.length() / 2; i++) {
-            boolean flag = false;
-            for (int j = i + 1; j < s.length(); j++) {
-                if (s.charAt(j) == s.charAt(j - i)) {
-                    flag = true;
-                }else{
-                    flag = false;
-                    break;
+
+    /**
+     * 花了半天学习了KMP，至于题解...不重要，反正我也想不到方法二。
+     * 还是网友的解释牛逼：https://www.zhihu.com/question/21923021，
+     * 还可以看这个视频作参考https://www.bilibili.com/video/av81151415?p=1 。
+     * 算法第四版
+     * ——需要系统学下KMP算法
+     * @param s
+     * @return
+     */
+    public boolean repeatedSubstringPattern1ByLeetCode(String s) {
+        int n = s.length();
+        for (int i = 1; i * 2 <= n; i++) {
+            // 重复子串必完整构成字符串
+            if (n % i == 0) {
+                boolean match = true;
+                for (int j = i; j < n; j++) {
+                    if (s.charAt(j) != s.charAt(j - i)) {
+                        match = false;
+                        break;
+                    }
+                }
+                if (match) {
+                    return true;
                 }
             }
-            if(flag){
-                return true;
-            }
         }
-
         return false;
     }
+
+    // ......KMP算法解法未完待续
 
     public static void main(String[] args) {
         RepeatedSubstringPattern459 repeatedSubstringPattern459 = new RepeatedSubstringPattern459();
 
         // aba不算
-        System.out.println(repeatedSubstringPattern459.repeatedSubstringPattern2("abab"));
-        System.out.println(repeatedSubstringPattern459.repeatedSubstringPattern2("ababab"));
-        System.out.println(repeatedSubstringPattern459.repeatedSubstringPattern2("abcabcabcabc"));
-        System.out.println(repeatedSubstringPattern459.repeatedSubstringPattern2("aabaaba"));
+        //        System.out.println(repeatedSubstringPattern459.repeatedSubstringPattern2("abab"));
+        //        System.out.println(repeatedSubstringPattern459.repeatedSubstringPattern2("ababab"));
+        //        System.out.println(repeatedSubstringPattern459.repeatedSubstringPattern2("abcabcabcabc"));
+//        System.out.println(repeatedSubstringPattern459.repeatedSubstringPattern2("aabaaba"));
+        System.out.println(repeatedSubstringPattern459.repeatedSubstringPattern("aabaaba"));
+        //        System.out.println(repeatedSubstringPattern459.repeatedSubstringPattern2("bb"));
     }
 }
